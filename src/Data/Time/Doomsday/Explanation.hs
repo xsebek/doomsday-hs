e722026f1babd0841562e42cf3015ae4f2aa7cdb
Ondřej Weis <ondras98@icloud.com>
2026-03-21 13:07:07 +0100

    Reorder declarations

--- src/Data/Time/Doomsday/Explanation.hs
+++ src/Data/Time/Doomsday/Explanation.hs
@@ -3,6 +3,7 @@ module Data.Time.Doomsday.Explanation (
     Explanation (..),
     Part (..),
     Step (..),
+    PartBuilder,
     step,
     part,
     startingWithYear,
@@ -13,13 +14,13 @@ import Data.Time.Doomsday.State.Simple
 import Data.Time.Doomsday.String.Pretty
 import Data.List (intercalate)
 
+---------------------------------------------------------------------
+-- DATA
+---------------------------------------------------------------------
+
 data Explanation = Explanation
   { parts :: [Part]
   }
-
-instance Pretty Explanation where
-  pretty :: Explanation -> String
-  pretty (Explanation ps) = intercalate "\n" $ map pretty (reverse ps)
 
 data Part = Part
   { goal :: String
@@ -27,17 +28,7 @@ data Part = Part
   , steps :: [Step]
   }
 
-instance Pretty Part where
-  pretty :: Part -> String
-  pretty (Part g s ss) = unlines $ (g <> " " <> pretty s) : map ((" - " <>) . pretty) (reverse ss)
-
 data StartDate = StartingWithYear Char | StartingWithDate
-
-instance Pretty StartDate where
-  pretty :: StartDate -> String
-  pretty = \case
-    StartingWithYear y -> "Starting with year " <> [y] <> ":"
-    StartingWithDate -> "Starting with date:"
 
 data Step = Step
   { description :: String
@@ -45,22 +36,44 @@ data Step = Step
   , expression :: Expression
   }
 
+data PartBuilder
+  = PartStartYear Char (State Part Expression)
+
+---------------------------------------------------------------------
+-- Pretty
+---------------------------------------------------------------------
+
+instance Pretty Explanation where
+  pretty :: Explanation -> String
+  pretty (Explanation ps) = intercalate "\n" $ map pretty (reverse ps)
+
+instance Pretty Part where
+  pretty :: Part -> String
+  pretty (Part g s ss) = unlines $ (g <> " " <> pretty s) : map ((" - " <>) . pretty) (reverse ss)
+
+instance Pretty StartDate where
+  pretty :: StartDate -> String
+  pretty = \case
+    StartingWithYear y -> "Starting with year " <> [y] <> ":"
+    StartingWithDate -> "Starting with date:"
+
 instance Pretty Step where
   pretty :: Step -> String
   pretty (Step d v e) = unwords [d, [v], "=", pretty e]
 
-step :: String -> Char -> Expression -> State Part Expression
-step description variable expression = State $ \p ->
-  (p { steps = Step description variable expression : steps p }, EVar variable)
-
-data PartBuilder
-  = PartStartYear Char (State Part Expression)
-
-startingWithYear :: Char -> (Expression -> State Part Expression) -> PartBuilder
-startingWithYear y b = PartStartYear y $ b (EVar y)
+---------------------------------------------------------------------
+-- Functions
+---------------------------------------------------------------------
 
 part :: String -> PartBuilder -> State Explanation Expression
 part goal = \case
   (PartStartYear y (State f)) -> State $ \expl ->
     case f (Part goal (StartingWithYear y) []) of
       (p, e) -> (expl { parts = p : parts expl }, e)
+
+startingWithYear :: Char -> (Expression -> State Part Expression) -> PartBuilder
+startingWithYear y b = PartStartYear y $ b (EVar y)
+
+step :: String -> Char -> Expression -> State Part Expression
+step description variable expression = State $ \p ->
+  (p { steps = Step description variable expression : steps p }, EVar variable)
